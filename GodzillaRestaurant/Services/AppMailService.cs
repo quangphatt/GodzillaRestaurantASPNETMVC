372fa44ab7d525bd45536d02b0ae642ba6d83554
Lai Quang Phat <77201894+quangphatt@users.noreply.github.com>
2023-11-20 00:17:00 +0700

    add mail sender

---
+++ GodzillaRestaurant/Services/AppMailService.cs
@@ -0,0 +1,43 @@
+﻿using GodzillaRestaurant.Models;
+using MailKit.Security;
+using MimeKit;
+
+namespace GodzillaRestaurant.Services
+{
+    public class AppMailService : IMailService
+    {
+        const string MAIL_HOST = "smtp.gmail.com";
+        const int MAIL_PORT = 587;
+
+        public AppMailService() { }
+
+        public async Task SendMail(MailContent mailContent)
+        {
+            var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+            var email = new MimeMessage();
+            email.Sender = MailboxAddress.Parse(config["EMAIL"]);
+            email.To.Add(MailboxAddress.Parse(mailContent.To));
+            email.Subject = mailContent.Subject;
+
+            var builder = new BodyBuilder();
+            builder.HtmlBody = mailContent.Body;
+            email.Body = builder.ToMessageBody();
+
+            // Use SmtpClient of MailKit
+            using var smtp = new MailKit.Net.Smtp.SmtpClient();
+
+            try
+            {
+                smtp.Connect(MAIL_HOST, MAIL_PORT, SecureSocketOptions.StartTls);
+                smtp.Authenticate(config["EMAIL"], config["EMAIL_PASSWORD"]);
+                await smtp.SendAsync(email);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
+
+            smtp.Disconnect(true);
+        }
+    }
+}
