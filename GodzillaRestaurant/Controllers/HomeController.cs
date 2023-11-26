8a2f1942350630a01ea2a00c77dcd3a61916182d
Lai Quang Phat <77201894+quangphatt@users.noreply.github.com>
2023-11-27 01:32:13 +0700

    admin role

--- GodzillaRestaurant/Controllers/HomeController.cs
+++ GodzillaRestaurant/Controllers/HomeController.cs
@@ -1,20 +1,29 @@
 ﻿using GodzillaRestaurant.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace GodzillaRestaurant.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<AppUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager)
         {
             _logger = logger;
+            this._userManager = userManager;
         }
 
         public IActionResult Index()
         {
+            if (this.User.IsInRole("ADMIN"))
+            {
+                return Redirect("Admin");
+            }
             return View();
         }
 
