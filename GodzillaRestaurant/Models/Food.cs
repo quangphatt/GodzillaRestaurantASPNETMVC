55467fbd8dce9fe6207ba550fcc72365030e9212
Lai Quang Phat <77201894+quangphatt@users.noreply.github.com>
2023-12-01 23:15:16 +0700

    food model

---
+++ GodzillaRestaurant/Models/Food.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GodzillaRestaurant.Models
+{
+    public class Food
+    {
+        [Key]
+        public int FoodId { get; set; }
+        public string FoodName { get; set; }
+        public string Description { get; set;}
+        public string FoodType { get; set;}
+        public int Price { get; set; }
+    }
+}
