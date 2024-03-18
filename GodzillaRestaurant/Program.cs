734f6a81db2e938586ad7ed819202efccfe2e2ae
Lai Quang Phat <77201894+quangphatt@users.noreply.github.com>
2024-03-18 22:56:04 +0700

    Error Page

--- GodzillaRestaurant/Program.cs
+++ GodzillaRestaurant/Program.cs
@@ -57,6 +57,16 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+app.Use(async (context, next) =>
+{
+    await next();
+    if (context.Response.StatusCode == 404)
+    {
+        context.Request.Path = "/Home";
+        await next();
+    }
+});
+
 app.UseStaticFiles();
 
 app.UseRouting();
